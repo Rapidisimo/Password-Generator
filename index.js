1ae3f8560d2bf4c0f9ba60e3272c20c1287ee1da
Josué Rodriguez <13837690+Rapidisimo@users.noreply.github.com>
2022-12-09 22:28:25 -0500

    working version

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
-const characters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+const characters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
+"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
+ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+ "~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
 
+const passBtn = document.getElementById('pswbtn');
+let firstPassword = document.getElementById('password1');
+let secondPassword = document.getElementById('password2');
+let passwordLength = 15;
 
+passBtn.addEventListener('click', () => {
+    firstPassword.textContent = '';
+    secondPassword.textContent = '';
+    for (let i = 0; i < passwordLength; i++) {
+        firstPassword.textContent += randomElement(characters);
+    }
+    for (let i = 0; i < passwordLength; i++) {
+        secondPassword.textContent += randomElement(characters);
+    }
+});
 
+function randomElement (array) {
+    return array[Math.floor( Math.random() * array.length )]
+}
 
